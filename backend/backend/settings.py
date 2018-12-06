1169ffe72d9b754f6b59a125f02b54c3d61d2e70
dlcjfgmlnasa <dlcjfgmlnasa@naver.com>
2018-12-06 17:44:01 +0900

    update setting.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+import environ
+
+ROOT_DIR = environ.Path(__file__) - 3  # (nomadgram/config/settings/base.py - 3 = nomadgram/)
+APPS_DIR = ROOT_DIR.path('backend')
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
